a7d71fc71ee719604ec862ead43190b18b9dcc4a
Jack Ryan Oracoy <jackryanoracoy@gmail.com>
2020-06-25 17:59:44 +0800

    Creating new custom component (Form)(WIP/Testing).

--- src/components/page-contact/index.js
+++ src/components/page-contact/index.js
@@ -1,21 +1,9 @@
-import React, { useEffect, useState } from 'react';
-import Button from '../site-button/';
+import React from 'react';
+import Form from '../site-form/';
 
 import './index.scss';
 
 function About() {
-
-  useEffect(() => {
-    fetchItems()
-  }, []);
-
-  const [items, setItems] = useState([]);
-
-  const fetchItems = async () => {
-    const data = await fetch('https://baconipsum.com/api/?type=all-meat&paras=3&start-with-lorem=1');
-    const items = await data.json();
-    setItems(items);
-  }
 
   return (
     <main className="contact">
@@ -31,20 +19,7 @@ function About() {
         <h2 className="sr-only">Contact Page</h2>
         <div className="container">
           <section className="section">
-            <h3 className="mb-20">This is a dummy content</h3>
-
-            {items.map(item => (
-              <p className="mb-40 mb-sm-20" key={item}>{item}</p>
-            ))}
-
-            <div className="flex gap-10">
-              <div className="flex-item-12 flex-item-sm">
-                <Button buttonClass="is-danger is-block" buttonText="Back To Home Page" />
-              </div>
-              <div className="flex-item-12 flex-item-sm">
-                <Button buttonClass="is-primary is-block" buttonPath="/about/" buttonText="Visit About Page" />
-              </div>
-            </div>
+            <Form />
           </section>
         </div>
       </article>
--- src/components/site-form/index.js
+++ src/components/site-form/index.js
@@ -5,9 +5,12 @@ import './index.scss';
 class Form extends Component {
   render() {
     return (
-      <div className="form">
-        <p>This is a Form</p>
-      </div>
+      <form className="form">
+        <input className="width-100 mb-20" type="text" placeholder="Input box 1" />
+        <input className="width-100 mb-20" type="text" placeholder="Input box 2" />
+        <input className="width-100 mb-20" type="text" placeholder="Input box 3" />
+        <button>Dummy Button</button>
+      </form>
     );
   }
 }
